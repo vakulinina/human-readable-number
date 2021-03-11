1ef9f61eead84422027b6f25db5488fb10a316f0
vakulinina <lerrun@gmail.com>
2021-03-11 16:35:20 +0300

    feat: implement toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+  const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+  function convertDozens(number) {
+    if (number < 10) {
+      return ones[number];
+    }
+    if (number >= 10 && number < 20) {
+      return teens[number - 10];
+    }
+    if (number >= 20) {
+      if (number % 10) {
+        return `${dozens[Math.floor(number / 10)]} ${ones[number % 10]}`;
+      }
+      return `${dozens[Math.floor(number / 10)]}`;
+    }
+  }
+
+  function convertHundreds(number) {
+    if (!number) return `zero`;
+    if (number > 99) {
+      if (number % 100) {
+        return `${ones[Math.floor(number / 100)]} hundred ${convertDozens(number % 100)}`;
+      }
+      return `${ones[Math.floor(number / 100)]} hundred`;
+    }
+    return convertDozens(number);
+  }
+
+  return convertHundreds(number);
 }
